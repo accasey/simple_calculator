51267a09fec69991db16a7d8c4b1bda07bd63781
Andrew Casey <accasey@hotmail.com.au>
2022-09-04 07:44:11 +0100

    Finished part 2

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -9,4 +9,26 @@ class SimpleCalculator:
         return a - b
 
     def mul(self, *args):
+        if not all(args):
+            raise ValueError
         return reduce(lambda x, y: x * y, args)
+
+    def div(self, a, b):
+        if not b:
+            return float("inf")
+
+        return a / b
+
+    def avg(self, it, lt=None, ut=None):
+        _it = it[:]
+
+        if lt is not None:
+            _it = [x for x in _it if x >= lt]
+
+        if ut is not None:
+            _it = [x for x in _it if x <= ut]
+
+        if not len(_it):
+            return 0
+
+        return sum(_it) / len(_it)
